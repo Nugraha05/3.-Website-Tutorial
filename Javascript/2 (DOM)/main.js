47c2f1548aa831bb2dda8a2bb73a64290acfc5f0
Nugraha05 <69519047+Nugraha05@users.noreply.github.com>
2023-08-14 17:03:18 +0700

    tes

---
+++ Javascript/2 (DOM)/main.js
@@ -0,0 +1,12 @@
+// const btn = document.getElementById('btn1')
+// const btn1 = document.querySelector('#btn1')
+
+function tes() {
+    var duitPertama = Number(prompt('Duit Awal'))
+    var duitKedua = Number(prompt('Duit Kedua'))
+    var hutang = 100000
+
+    var duitAkhir = duitPertama + duitKedua - hutang
+
+    console.log(duitAkhir)
+}
