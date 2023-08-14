6f621fad1cb27215f8f0aec23dcdb8c1ac33a1e0
Nugraha05 <69519047+Nugraha05@users.noreply.github.com>
2023-08-14 17:39:53 +0700

    tes

--- Javascript/2 (DOM)/main.js
+++ Javascript/2 (DOM)/main.js
@@ -1,4 +1,4 @@
-// const body = document.body
+const body = document.body
 // const btn1 = document.querySelector('#btn1')
 
 
@@ -18,4 +18,26 @@ function tes() {
 
     // Console Information
     console.log(duitAkhir)
+}
+
+function table() {
+    var nama = prompt('Nama:')
+    var sekolah = prompt('Sekolah')
+    var umur = prompt('Umur')
+
+    // const tesBang = document.querySelector('tr')
+    // tesBang.innerHTML = '<tr><td>tes</td><td>tes</td><td>tes</td></tr>'
+    // body.append(tesBang)
+
+    const namaHasil = document.querySelector('#nama')
+    namaHasil.innerHTML = nama
+    console.log(namaHasil)
+
+    const sekolahHasil = document.querySelector('#sekolah')
+    sekolahHasil.innerHTML = sekolah
+    console.log(sekolahHasil)
+
+    const umurHasil = document.querySelector('#umur')
+    umurHasil.innerHTML = umur
+    console.log(umurHasil)
 }
